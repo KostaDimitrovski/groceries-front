990b2d127365a68f73412914250faa9b779945fd
Kosta_Dimitrovski <dimitrovskikosta2@gmail.com>
2024-06-30 12:22:25 +0200

    Initial config

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {Routes, Route} from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+       <Routes>
+           <Route path="/" element={<HomePage/>}/>
+           <Route path="/Login" element={<Login/>}/>
+           <Route path="/Register" element={<Register/>}/>
+       </Routes>
+   </>
   );
 }
 
